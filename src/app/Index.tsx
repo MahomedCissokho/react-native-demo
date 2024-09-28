28feb194885d1295154137ad86ce0ed9213e3702
Mahomed CISSOKHO <mahomedcissokho@esp.sn>
2024-09-28 22:30:53 +0000

    done challenge

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -5,16 +5,12 @@ import Handle from "../components/Handle";
 import Subject from "../components/Subject";
 import ButtonComponent from "../components/ButtonComponent";
 import ImageComponent from "../components/ImageComponent";
+import Card from "../components/Card";
 
 const Index = () => {
   return (
     <View>
-      <Greet />
-      <Age />
-      <Handle />
-      <Subject />
-      <ButtonComponent/>
-      <ImageComponent/>
+      <Card/>
     </View>
   );
 };
