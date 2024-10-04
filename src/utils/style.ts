e028a906284134f6c34cf0cefd2743942e29bd77
Mahomed CISSOKHO <mahomedcissokho@esp.sn>
2024-10-04 12:18:56 +0000

    last thing to adjust

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -20,6 +20,14 @@ const externalStyle  = StyleSheet.create({
         borderWidth : 2,
         borderColor : 'black',
         borderRadius : 10,
+        shadowColor: '#fff',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
     secondViewStyle: {
         width : 200,
